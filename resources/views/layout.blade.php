9b5918248104f922e090aa2a9cfbaf243be05c7c
bjamyl <alhassanjamil0@gmail.com>
2023-03-08 23:59:34 +0000

    created components

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -27,9 +27,9 @@
         <title>LaraGigs | Find Laravel Jobs & Projects</title>
     </head>
     <body class="mb-48">
-        <nav class="flex justify-between items-center mb-4">
-            <a href="index.html"
-                ><img class="w-24" src="{{assets('images/logo.png')}}" alt="" class="logo"
+        <nav class="flex  justify-between items-center mb-4">
+            <a href="/"
+                ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
